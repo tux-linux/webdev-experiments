b40e3390b7b4a4aa0b3cfebd187d4d1a88bb1b2e
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-08-30 20:16:58 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import { MDCRipple } from "@material/ripple/index";
+
+function helloWorld() {
+    console.log("Hello world");
+}
+
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
